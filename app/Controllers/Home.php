5280b874f3af8dba352f2d02b433bdd0dac96b85
Adji Putra Nugraha <93044810+UjangCode@users.noreply.github.com>
2022-01-13 06:50:41 +0700

    update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,21 @@
 
 namespace App\Controllers;
 
+use App\Models\EmployeeModel;
+use CodeIgniter\API\ResponseTrait;
+
 class Home extends BaseController
 {
+    use ResponseTrait;
+
+    function __construct()
+    {
+        $this->EmployeeModel = new EmployeeModel();
+    }
     public function index()
     {
-        // return view('welcome_message');
+        // $db = $this->EmployeeModel->where('id','2')->first();
+        // return view('home');
+        return view('home');
     }
 }
